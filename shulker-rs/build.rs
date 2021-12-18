df8c0ffa28f66d34915bd53a3c92499b4b5c578e
AlexanderHeim <47790717+AlexanderHeim@users.noreply.github.com>
2021-12-18 16:22:06 +0100

    init ui

---
+++ shulker-rs/build.rs
@@ -0,0 +1,3 @@
+fn main() {
+    sixtyfps_build::compile("ui/main_ui.60").unwrap();
+}
--- shulker-rs/src/bin/main.rs
+++ shulker-rs/src/bin/main.rs
@@ -1,35 +1,17 @@
 // ONLY FOR TESTING PURPOSES
 
-use std::{env, path::PathBuf};
+use std::{env, path::PathBuf, sync::mpsc::channel};
 use shulker_rs::{credential_types::{Credential, Secret}, shulker_db::ShulkerDB};
 use uuid::Uuid;
 
-
+sixtyfps::include_modules!();
 fn main() {
-    let current_dir = env::current_dir().unwrap();
-    let mut shulker_db = ShulkerDB::new(PathBuf::from("credentials.skr"));
-
-    let credential_1 = Credential {
-        uuid: Uuid::new_v4(),
-        time_frame: None,
-        uses_left: None,
-        secret: Secret::PinCode(String::from("32142")),
-    };
-    let credential_2 = Credential {
-        uuid: Uuid::new_v4(),
-        time_frame: None,
-        uses_left: None,
-        secret: Secret::Password(String::from("alexander")),
-    };
-    let credential_3 = Credential {
-        uuid: Uuid::new_v4(),
-        time_frame: None,
-        uses_left: None,
-        secret: Secret::Password(String::from("449")),
-    };
-    shulker_db.add(credential_1).unwrap();
-    shulker_db.add(credential_2).unwrap();
-    shulker_db.add(credential_3).unwrap();
-
-    println!("{:#?}", shulker_db.get_all(None));
+    let (tx, rx) = channel();
+    let handler = std::thread::spawn(move || {
+        println!("{:#?}", rx.recv());
+        
+    });
+    tx.send("TEST").unwrap();
+    MainUi::new().run();
+    handler.join().unwrap();
 }
