328f62ec7de014d0c4c0e57ab8668392a8f3ca64
Moritz-github <47506586+Moritz-github@users.noreply.github.com>
2021-12-25 15:10:25 +0100

    pin manager expandable tiles

--- client/lib/views/PinManager.dart
+++ client/lib/views/PinManager.dart
@@ -1,6 +1,7 @@
 import 'package:doorlock_app/models/Credential.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class PinManager extends StatefulWidget {
   @override
@@ -18,34 +19,36 @@ class _PinManagerState extends State<PinManager> {
         child: ListView.builder(
             itemCount: exampleCreds.length,
             itemBuilder: (context, index) {
-              return ListTile(
+              return Card(
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(15)
-                ),
-                  leading: const Text("****"),
-                  title: const Text("test"),
-                  trailing: PopupMenuButton(
-                    itemBuilder: (context) => [
-                      PopupMenuItem(
-                        child: Text("Bearbeiten"),
-                        value: 0,
+                    borderRadius: BorderRadius.circular(15)),
+                child: ExpansionTile(
+                    childrenPadding: EdgeInsets.all(15).copyWith(top: 0),
+                    leading: const Icon(Icons.password),
+                    title: Text(exampleCreds[index].label),
+                    children: [
+                      TextField(
+                        controller: TextEditingController()
+                          ..text = exampleCreds[index].usesLeft.toString(),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: [
+                          FilteringTextInputFormatter.digitsOnly,
+                        ],
+                        decoration: InputDecoration(
+                          labelText: 'Verbleibende Verwendungen',
+                          hintText: '',
+                          border: InputBorder.none,
+                        ),
                       ),
-                      PopupMenuItem(
-                        child: Text("Löschen"),
-                        value: 1,
-                      )
-                    ],
-                    onSelected: (index) {
-                      print(index);
-                      if (index == 0) {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) {
-                              return EditPin();
-                            }));
-                      }
-                    },
-                  ),
-                  onTap: () {});
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Text("Gültig ab: " + exampleCreds[index].startDateTime.toString()),
+                          Text("Gültig bis: " + exampleCreds[index].endDateTime.toString()),
+                        ],
+                      ),
+                    ]),
+              );
             }),
       ),
     );
