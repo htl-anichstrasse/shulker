5e5dc54e6482a1960f7a672c6819d1debdc7eb28
alexanderheim <47790717+AlexanderHeim@users.noreply.github.com>
2022-03-21 20:33:37 +0100

    build done

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ if ! [ -x "$(command -v cargo)" ]; then
   exit 1
 fi
 
+if ! [ -x "$(command -v dotnet)" ]; then
+  echo 'Dotnet is not installed! Please install using install scripts from microsoft.'
+  exit 1
+fi
+
 # build core
 cd ./shulker-core
 cargo build --release
@@ -13,3 +18,9 @@ cp ./shulker-core/target/release/shulker-core ./core
 rm -rf ./shulker-core/target
 
 # build connect
+cd ./shulker-connect/DoorlockServerAPI
+dotnet build --configuration Release
+cd ../..
+cp -r ./shulker-connect/DoorlockServerAPI/bin/Release/net5.0 ./connect
+
+#run DoorlockServerAPI with --urls "http://10.0.0.154:42001"
